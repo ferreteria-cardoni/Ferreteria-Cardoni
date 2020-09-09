3732dd797921b702923151bf073fb8a2d001ea5f
MardoCordova <70610290+MardoCordova@users.noreply.github.com>
2020-09-08 22:06:59 -0600

    Validaciones del formulario de Productos
    archivo de mensajes en español, js de validaciones algunos cambios sobre la vista(span para mansajes y un alert)

---
+++ app/Http/Requests/FormProductoIngresar.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FormProductoIngresar extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'idnombre' => 'required',
+            'idmarca' => 'required',
+            'idpresentacion' => 'required',
+            'idcantidad' => 'required|numeric',
+            'idprecio' => 'required|numeric',
+            'idproveedor' => 'required',
+            
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'idnombre.required' => 'El Campo Nombre no puede estar vacio',
+            'idmarca.required' => 'Se debe seleccionar almenos una opcione del Campo Marca',
+            'idpresentacion.required' => 'El Campo Presentacion no puede estar vacio',
+            'idcantidad.required' => 'El Campo Cantidad debe tener almenos un valor de 0',
+            'idprecio.required' => 'El Campo Precio no puede tener un valor inferior a 0.01',
+            'idproveedor.required' => 'Se debe seleccionar almenos una opcione del Campo Proveedor',
+            
+        ];
+    }
+}
