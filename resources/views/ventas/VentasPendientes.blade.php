9033f6f2b543d71039643bb11cfbc649d7b7e338
Hiro <irvinn_tobias@hotmail.com>
2020-11-02 00:41:02 -0600

    probando

--- resources/views/ventas/VentasPendientes.blade.php
+++ resources/views/ventas/VentasPendientes.blade.php
@@ -16,30 +16,6 @@
 @endsection
 
 @section('content')
-<<<<<<< HEAD
-<div class="input-group-prepend">
-  <input class="form-control mr-sm-2" name="textoVentaP" id="textoVentaP" type="text" placeholder="Buscar Ventas" aria-label="Search">
-</div>
-<br>
-<div class="row">
-@foreach ($pedidoVentas as $pedido)
-<div class="col-sm-4" id="ok1">
-<div class="card">
-<div class="card-body">
-  <h5 class="card-title">Special title treatment</h5>
-  <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-  <ul class="list-group list-group-flush">
-      <li class="list-group-item">Vendido por: {{App\empleado::find($pedido->cod_empleado_fk)->nombre}} {{App\empleado::find($pedido->cod_empleado_fk)->apellido}}</li>
-      <li class="list-group-item">Cliente: {{App\cliente::find($pedido->cod_cliente_fk)->nombre}} {{App\cliente::find($pedido->cod_cliente_fk)->apellido}}</li>
-      <li class="list-group-item">Direccion: {{$pedido->direccion}}</li>
-      <li class="list-group-item">Total: ${{$pedido->total}}</li>
-  </ul>
-  <a href="{{route('Ventas.edit', $pedido->cod_venta)}}" class="btn btn-primary">Editar</a>
-</div>
-</div>
-</div>
-@endforeach
-=======
     <div class="input-group-prepend">
             <input class="form-control mr-sm-2" name="textoVentaP" id="textoVentaP" type="text" placeholder="Buscar Ventas" aria-label="Search">
     </div>
@@ -85,6 +61,5 @@
         </tr>
       </tbody>
   </table> -->
->>>>>>> 21ac99d436716d267207108cd154d54c9aacd086
 </div>
 @endsection
