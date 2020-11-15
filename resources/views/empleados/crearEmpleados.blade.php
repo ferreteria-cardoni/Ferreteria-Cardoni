bfeca7e68ffe0fd59e9c500bea96b35ddf27f592
Hiro <irvinn_tobias@hotmail.com>
2020-11-15 00:37:33 -0600

    registrar usuarios funcionando

--- resources/views/empleados/crearEmpleados.blade.php
+++ resources/views/empleados/crearEmpleados.blade.php
@@ -14,122 +14,133 @@
 		</button>
 	</div>
 	@endif
-@endsection
+	@endsection
 
-@section('alert2')
-<div class="container">
-	@if (session('datosError'))
-	<div class="alert alert-danger alert-dismissible fade show" role="alert" align="center">
-		{{session('datosError')}}
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-			<span aria-hidden="true">&times;</span>
-		</button>
-	</div>
-	@endif
-@endsection
+	@section('alert2')
+	<div class="container">
+		@if (session('datosError'))
+		<div class="alert alert-danger alert-dismissible fade show" role="alert" align="center">
+			{{session('datosError')}}
+			<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				<span aria-hidden="true">&times;</span>
+			</button>
+		</div>
+		@endif
+		@endsection
 
 
 @section('content')
 
-@if ($errors->any())
-	<div class="alert alert-danger">
-		<center>
-			<h5>Hay errores en en formulario, favor revisar</H2>
-		</center>
-		<ul>
-			@foreach ($errors->all() as $error)
-			<li>{{ $error }}</li>
-			@endforeach
-		</ul>
-	</div>
-@endif
-
-
-
-<form method="POST" action="{{route('Empleados.store')}}" >
-	@csrf
-	  <div class="form-row">
-				  	<div class="form-group col-md-5">
-				      <label>Nombre</label>
-				      <input autocomplete="off" type="text" class="form-control" name="NombreEmpleado" id="NombreEmpleado" placeholder="Escriba el nombre..." value="{{ old('NombreEmpleado') }}" required>
-              <span id="msgNombreEmpleado" name="msgNombreEmpleado" class="AlertaMsg"></span>
-				    </div>
-
-				    <div class="form-group col-md-5">
-						<label>Apellido</label>
-						<input type="text" class="form-control" id="ApellidoEmpleado" name="ApellidoEmpleado" placeholder="Escriba el apellido..." value="{{ old('ApellidoEmpleado') }}" required>
-						<span id="msgApellidoEmpleado" name="msgApellidoEmpleado" class="AlertaMsg"></span>
-					</div>
-		<div class="form-group col-md-5">
-		<label>DUI</label>
-		<input type="numeric" class="form-control" id="DUIE" name="DUIE" maxlength="9" placeholder="Escriba el DUI..."
-			value="{{ old('DUIE') }}" required>
-		<span id="msgDUIE" name="msgDUIE" class="AlertaMsg"></span>
-	</div>
-
-	<div class="form-group col-md-5">
-				      <label>Edad</label>
-				      <input autocomplete="off" type="number" min="18" class="form-control" maxlength="6" minlength="6" name="idEdadE" id="idEdadE" placeholder="Escriba la edad..." value="{{ old('idEdadE') }}" required>
-              <span id="msgidEdadE" name="msgidEdadE" class="AlertaMsg"></span>
-				    </div>
-
-	<div class="form-group col-md-5">
-		<label>Sexo</label>
-      	<select class="browser-default custom-select" id="sexoE" name="sexoE" required>
-			<option value="0" selected> Seleccione</option>
-			<option value="masculino">Masculino</option>
-			<option value="femenino">Femenino</option>
-			<option value="otro">Otro</option>
-		</select>
-		<span id="msgsexoE" name="msgsexoE" class="AlertaMsg"></span>
-	</div>
-
-
-
-		<div class="form-group col-md-5">
-			<label>Teléfono</label>
-			<input type="text" class="form-control" id="idtelefonoE" name="idtelefonoE"
-				placeholder="Escribe el teléfono..." maxlength="8" value="{{ old('idtelefonoProve') }}" required>
-			<span id="msgidtelefonoE" name="msgidtelefonoE" class="AlertaMsg"></span>
-		</div>
-
-		<div class="form-group col-md-5">
-			<label>Correo electrónico</label>
-			<input type="text" class="form-control" id="idcorreoE" name="idcorreoE" placeholder="Email..."
-				value="{{ old('idcorreoE') }}">
-			<span id="msgidcorreoE" name="msgidcorreoE" class="AlertaMsg"></span>
-		</div>
-
-
-		<div class="form-group col-md-5">
-			<label>Rol</label>
-			<select class="browser-default custom-select" id="idrol" name="idrol" required>
-				<option value="0" selected disabled> Seleccione un rol</option>
-				@foreach ($roles as $rol)
-				<option value="{{$rol->id}}">{{$rol->nombre}}</option>
+		@if ($errors->any())
+		<div class="alert alert-danger">
+			<center>
+				<h5>Hay errores en en formulario, favor revisar</H2>
+			</center>
+			<ul>
+				@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
 				@endforeach
-			</select>
-			<span id="msgsexoE" name="msgsexoE" class="AlertaMsg"></span>
+			</ul>
 		</div>
+		@endif
 
 
 
+		<form method="POST" action="{{route('Empleados.store')}}">
+			@csrf
+			<h3>Datos del empleado</h3>
 
-		{{-- <div class="form-group col-md-5">
-			<label for="idrol" class="mb-2">Rol</label>
-			<select class='mi-selector' name='idrol[]' id="idrol" multiple='multiple' required>
-				<option disabled="true">Seleccione el Rol </option>
-			</select>
-		</div> --}}
+			<div class="form-row">
+				<div class="form-group col-md-5">
+					<label>Nombre</label>
+					<input autocomplete="off" type="text" class="form-control" name="NombreEmpleado" id="NombreEmpleado"
+						placeholder="Escriba el nombre..." value="{{ old('NombreEmpleado') }}" required>
+					<span id="msgNombreEmpleado" name="msgNombreEmpleado" class="AlertaMsg"></span>
+				</div>
+
+				<div class="form-group col-md-5">
+					<label>Apellido</label>
+					<input type="text" class="form-control" id="ApellidoEmpleado" name="ApellidoEmpleado"
+						placeholder="Escriba el apellido..." value="{{ old('ApellidoEmpleado') }}" required>
+					<span id="msgApellidoEmpleado" name="msgApellidoEmpleado" class="AlertaMsg"></span>
+				</div>
+				<div class="form-group col-md-5">
+					<label>DUI</label>
+					<input type="numeric" class="form-control" id="DUIE" name="DUIE" maxlength="9"
+						placeholder="Escriba el DUI..." value="{{ old('DUIE') }}" required>
+					<span id="msgDUIE" name="msgDUIE" class="AlertaMsg"></span>
+				</div>
+
+				<div class="form-group col-md-5">
+					<label>Edad</label>
+					<input autocomplete="off" type="number" min="18" class="form-control" maxlength="6" minlength="6"
+						name="idEdadE" id="idEdadE" placeholder="Escriba la edad..." value="{{ old('idEdadE') }}"
+						required>
+					<span id="msgidEdadE" name="msgidEdadE" class="AlertaMsg"></span>
+				</div>
+
+				<div class="form-group col-md-5">
+					<label>Sexo</label>
+					<select class="browser-default custom-select" id="sexoE" name="sexoE" required>
+						<option value="0" selected> Seleccione</option>
+						<option value="masculino">Masculino</option>
+						<option value="femenino">Femenino</option>
+						<option value="otro">Otro</option>
+					</select>
+					<span id="msgsexoE" name="msgsexoE" class="AlertaMsg"></span>
+				</div>
 
 
-		
-</div>
 
-<button type="submit" class="btn btn-primary">Registrar Empleado</button>
-<button type="reset" class="btn btn-danger">Limpiar Campos</button>
+				<div class="form-group col-md-5">
+					<label>Teléfono</label>
+					<input type="text" class="form-control" id="idtelefonoE" name="idtelefonoE"
+						placeholder="Escribe el teléfono..." maxlength="8" value="{{ old('idtelefonoProve') }}"
+						required>
+					<span id="msgidtelefonoE" name="msgidtelefonoE" class="AlertaMsg"></span>
+				</div>
 
-</form>
+
+				<h3 class="form-group col-md-12"><hr>Datos del usuario</h3>
+				
+
+				<div class="form-group col-md-5">
+					<label>Correo electrónico</label>
+					<input type="email" class="form-control" id="idcorreoE" name="idcorreoE" placeholder="Email..."
+						value="{{ old('idcorreoE') }}">
+					<span id="msgidcorreoE" name="msgidcorreoE" class="AlertaMsg"></span>
+				</div>
+
+
+				<div class="form-group col-md-5">
+					<label>Rol</label>
+					<select class="browser-default custom-select" id="idrol" name="idrol" required>
+						<option value="0" selected disabled> Seleccione un rol</option>
+						@foreach ($roles as $rol)
+						<option value="{{$rol->id}}">{{$rol->nombre}}</option>
+						@endforeach
+					</select>
+					<span id="msgsexoE" name="msgsexoE" class="AlertaMsg"></span>
+				</div>
+
+				<div class="form-group col-md-5">
+					<label for="idcontraseña1">Contraseña</label>
+					<input id="idcontraseña1" name="idcontraseña1" class="form-control" min="8" type="password" placeholder="Escriba la contraseña..." required>
+					<span id="msgidcontraseña1" name="msgidcontraseña1" class="AlertaMsg"></span>
+				</div>
+
+				<div class="form-group col-md-5">
+					<label for="idcontraseña2">Confirmar contraseña</label>
+					<input id="idcontraseña2" name="idcontraseña2" class="form-control" min="8" type="password" placeholder="Escriba la contraseña..." required>
+					<span id="msgidcontraseña2" name="msgidcontraseña2" class="AlertaMsg"></span>
+				</div>
+
+			</div>
+
+			<button type="submit" class="btn btn-primary mb-4">Registrar</button>
+			<button type="reset" class="btn btn-danger mb-4">Limpiar Campos</button>
+
+		</form>
 
 
 @endsection
