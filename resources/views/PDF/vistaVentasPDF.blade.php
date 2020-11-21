728588277f2ab87daa4b59e6084ce3c76add4c0a
Hiro <irvinn_tobias@hotmail.com>
2020-11-21 02:00:18 -0600

    ya se generan pdf de movimientos de compras y ventas segun la fecha indicada

---
+++ resources/views/PDF/vistaVentasPDF.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('titulo','Generar PDF de Ventas')
+
+
+@section('content')
+<form action="{{route('pdfventas')}}" method="POST" target="_blank">
+    @csrf
+    <div class="form-row">
+        <div class="form-group col-md-5">
+            <label for="example-date-input">Fecha de inicio</label>
+            <input name="finicio" class="form-control" type="date" id="example-date-input" required>
+        </div>
+
+        <div class="form-group col-md-5">
+            <label for="example-date-input">Fecha final</label>
+            <input name="ffinal" class="form-control" type="date" id="example-date-input" required>
+        </div>
+    </div>
+
+    <button class="btn btn-primary" type="submit">Generar PDF</button>
+</form>
+@endsection
